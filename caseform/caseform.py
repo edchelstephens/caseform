1ad272d07f19707bc71a98b300556d662978f172
Edchel Stephen <edchelstephens@gmail.com>
2021-03-04 09:27:17 +0800

    Let transform return the new transformed mapping.

--- caseform/caseform.py
+++ caseform/caseform.py
@@ -4,26 +4,28 @@ from config.settings import TRANSFORM_CALLABLES
 from config.messages import invalid_transform_message
 
 def transform(mapping, func, *args, **kwargs):
-	"""Transform mapping to specified key case via func transformation callable."""
+	"""Returns a transformed case mapping via case transformation func callable."""
 	try:	
 		if func not in TRANSFORM_CALLABLES:
 			raise ValueError(invalid_transform_message())
 
 		_mapping = dict()
+		
 		for key, value in mapping.items():
-			trans_key = func(key, *args, **kwargs)
+			trans_key = func(key, *args, **kwargs)			
 			_mapping[trans_key] = value
-		mapping = _mapping
+
+		return _mapping
 
 	except Exception as exc:
 		raise exc
 
 def camelform(mapping, uppercase_first_letter=False):
 	"""Transform a mapping object to it's equivalent camelcase keyed mapping."""
-	transform(mapping, camelize, uppercase_first_letter)
+	return transform(mapping, camelize, uppercase_first_letter)
 
 def snakeform(mapping):
 	"""Transforms a mapping object to it's equivalent snakecase keyed mapping"""
-	transform(mapping, underscore)
+	return transform(mapping, underscore)
 
 
